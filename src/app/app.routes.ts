93232bd334faba83528d19e2dc747e33c217023a
Mateo Lituma <milituma@sudamericano.edu.ec>
2024-07-23 11:30:14 -0500

    folder changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Pantalla1Component } from '../components/pantalla1/pantalla1.component';
-import { Pantalla2Component } from '../components/pantalla2/pantalla2.component';
-import { Pantalla3Component } from '../components/pantalla3/pantalla3.component';
+import { Pantalla1Component } from '../pages/pantalla1/pantalla1.component';
+import { Pantalla2Component } from '../pages/pantalla2/pantalla2.component';
+import { Pantalla3Component } from '../pages/pantalla3/pantalla3.component';
 
 export const routes: Routes = [
     {
